f20ea839bee5a0191599126cc11cd89c1f387347
fcoverdugoa <fcoverdugoa@underpost.net>
2021-07-07 22:12:19 -0400

    update

--- json.php
+++ json.php
@@ -1,13 +1,14 @@
 <?php
 
 $path_email = "c:/xampp/htdocs/online/somosindia/sv/iframes/talleres/respira/api/usersRegister.json";
+$path_time = "c:/xampp/htdocs/online/somosindia/sv/iframes/talleres/respira/api/email_24horas.json";
 
 $emails = json_decode(file_get_contents($path_email), true);
 
 $newObj = new \stdClass();
 $newObj->name = "name";
 $newObj->email = "email";
-$newObj->time = 234234234;
+$newObj->time = json_decode(file_get_contents($path_time), true)["time"];
 $newObj->send = false;
 
 array_push($emails, $newObj);
